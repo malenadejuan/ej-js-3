93a98e62acc09a1c9fe894584d1be8c924834941
Malena de Juan <malena.dj4@gmail.com>
2021-02-22 10:20:42 +0100

    corrección clase

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,9 +1,29 @@
 function analizaPalabras(palabras) {
+  let analisis = [];
+  for (let i in palabras) {
+    const palabra = palabras[i];  /* solo existe dentro del for. */
+    analisis.push(`#######`);
+    analisis.push(`Palabra ${+i + 1}: ${palabra}`);  /* +i para forzarlo a que sea un numero. Es originalmente un string */
+    analisis.push(`Nº de caracteres: ${palabra.length}`);
+    analisis.push(`Deletreo: ${deletreo(palabra)}`);
+    analisis.push(`La palabra es ${esPar(palabra) ? "par" : "impar"}` +
+      ` y ${empiezaMayuscula(palabra) ? "" : "no "}empieza por mayúscula`);
+    analisis.push(`La palabra ${contieneBle(palabra) ? "" : "no "}contiene "ble"`);
+  }
+  return analisis.join("\n"); /* \n = caracter salto de linea */
+}
+
+const deletreo = texto => texto.split("").join("-");
+const esPar = texto => texto.length % 2 === 0;
+const empiezaMayuscula = texto => texto[0] === texto[0].toUpperCase();
+const contieneBle = texto => texto.toLowerCase().includes("ble"); /* el string que nos han pasado lo paso a LowerCase y ya puedo ver si contiene ble */
+/*
+function analizaPalabras(palabras) {
   palabras.forEach(indexPalabras);
 }
 
 function indexPalabras(palabra, index) {
-  console.log(`Palabra ${index}: ${palabra}`);
+  console.log(`Palabra ${ index }: ${ palabra }`);
   caracteres(palabra);
   deletrear(palabra);
   parMayus(palabra);
@@ -11,12 +31,12 @@ function indexPalabras(palabra, index) {
 }
 
 function caracteres(palabra) {
-  console.log(`Nº de caracteres: ${palabra.length}`);
+  console.log(`Nº de caracteres: ${ palabra.length }`);
 }
 
 function deletrear(palabra) {
   let deletreo = palabra.split("").join(" - ");
-  console.log(`Deletreo: ${deletreo}`);
+  console.log(`Deletreo: ${ deletreo }`);
 }
 
 function parMayus(palabra) {
@@ -28,7 +48,7 @@ function parMayus(palabra) {
   if (palabra.charAt(0) === palabra.charAt(0).toUpperCase()) {
     esMayus = " "
   } else { esMayus = " no " }
-  console.log(`La palabra es ${parOImpar} y${esMayus}empieza por mayúscula`);
+  console.log(`La palabra es ${ parOImpar } y${ esMayus }empieza por mayúscula`);
 }
 
 function contieneBle(palabra) {
@@ -38,16 +58,16 @@ function contieneBle(palabra) {
   } else {
     ble = " no ";
   }
-  console.log(`La palabra${ble}contiene "ble"`);
+  console.log(`La palabra${ ble }contiene "ble"`);
   console.log(`#######`);
 }
+ */
 
-
-/* console.log(`Palabra ${index}: ${palabra}`);
-console.log(`Nº de caracteres: ${caracteres}`);
-console.log(`Deletreo: ${deletreo}`);
-console.log(`La palabra es ${parOImpar} y ${esMayus} empieza por mayúscula`);
-console.log(`La palabra${ble}contiene "ble"`);
+/* console.log(`Palabra ${ index }: ${ palabra }`);
+console.log(`Nº de caracteres: ${ caracteres }`);
+console.log(`Deletreo: ${ deletreo }`);
+console.log(`La palabra es ${ parOImpar } y ${ esMayus } empieza por mayúscula`);
+console.log(`La palabra${ ble }contiene "ble"`);
  */
 
 /* Palabra 1: hola
